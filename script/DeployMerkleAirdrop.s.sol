b6c15a5b6d8b32d8e567e6fc8b2fcbb4af1804b1
max <Wht2826920647@outlook.com>
2025-12-02 17:06:49 +0800

    dev: add signature feature

---
+++ script/DeployMerkleAirdrop.s.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+import {Script} from "forge-std/Script.sol";
+import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
+import {BagelToken} from "../src/BagelToken.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract DeployMerkleAirdrop is Script {
+    bytes32 public constant ROOT =
+        0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+
+    uint256 public constant AMOUNT_TO_MINT = 25 * 1e18 * 4;
+
+    function run() public returns (MerkleAirdrop, BagelToken) {
+        return deployAirdrop();
+    }
+
+    function deployAirdrop() internal returns (MerkleAirdrop, BagelToken) {
+        vm.startBroadcast();
+        BagelToken bagelToken = new BagelToken();
+        MerkleAirdrop merkleAirdrop = new MerkleAirdrop(
+            ROOT,
+            IERC20(address(bagelToken))
+        );
+        bagelToken.mint(bagelToken.owner(), AMOUNT_TO_MINT);
+        bagelToken.transfer(address(merkleAirdrop), AMOUNT_TO_MINT);
+        vm.stopBroadcast();
+
+        return (merkleAirdrop, bagelToken);
+    }
+}
--- test/MerkleAirdrop.t.sol
+++ test/MerkleAirdrop.t.sol
@@ -4,28 +4,39 @@ pragma solidity ^0.8.30;
 import {Test, console} from "forge-std/Test.sol";
 import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
 import {BagelToken} from "../src/BagelToken.sol";
+import {ZkSyncChainChecker} from "@foundry-devops/ZkSyncChainChecker.sol";
+import {DeployMerkleAirdrop} from "../script/DeployMerkleAirdrop.s.sol";
 
-contract MerkleAirdropTest is Test {
+contract MerkleAirdropTest is ZkSyncChainChecker, Test {
     MerkleAirdrop public merkleAirdrop;
     BagelToken public bagelToken;
+    DeployMerkleAirdrop public deployer;
 
     bytes32 public Root =
         0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
 
     bytes32[] public PROOF;
 
+    address public gasPayer;
     address public user;
     uint256 public userPrivateKey;
     uint256 private constant AMOUNT_TO_CLAIM = 25 * 1e18;
     uint256 private constant AMOUNT_TO_MINT = AMOUNT_TO_CLAIM * 4;
 
     function setUp() public {
-        bagelToken = new BagelToken();
-        merkleAirdrop = new MerkleAirdrop(Root, bagelToken);
+        if (isZkSyncChain()) {
+            deployer = new DeployMerkleAirdrop();
+            (merkleAirdrop, bagelToken) = deployer.run();
+        } else {
+            bagelToken = new BagelToken();
+            merkleAirdrop = new MerkleAirdrop(Root, bagelToken);
 
-        bagelToken.mint(address(merkleAirdrop), AMOUNT_TO_MINT);
+            bagelToken.mint(bagelToken.owner(), AMOUNT_TO_MINT);
+            bagelToken.transfer(address(merkleAirdrop), AMOUNT_TO_MINT);
+        }
+
         (user, userPrivateKey) = makeAddrAndKey("user");
-
+        (gasPayer, ) = makeAddrAndKey("gasPayer");
         PROOF.push(
             0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a
         );
@@ -37,9 +48,11 @@ contract MerkleAirdropTest is Test {
     function testClaim() public {
         uint256 startingBalance = bagelToken.balanceOf(user);
 
-        vm.startPrank(user);
-        merkleAirdrop.claim(user, AMOUNT_TO_CLAIM, PROOF);
-        vm.stopPrank();
+        bytes32 digest = merkleAirdrop.getDigest(user, AMOUNT_TO_CLAIM);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(userPrivateKey, digest);
+
+        vm.prank(gasPayer);
+        merkleAirdrop.claim(user, AMOUNT_TO_CLAIM, PROOF, v, r, s);
 
         uint256 endingBalance = bagelToken.balanceOf(user);
         assertEq(endingBalance - startingBalance, AMOUNT_TO_CLAIM);
